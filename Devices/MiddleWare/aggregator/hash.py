177b472a2d460b6a75db7660ae4f88c94936242d
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2024-02-19 10:38:35 +0100

    UPD

--- Devices/MiddleWare/aggregator/hash.py
+++ Devices/MiddleWare/aggregator/hash.py
@@ -1,6 +1,5 @@
-# Adjusted MiMC implementation to match the ZoKrates example
+import numpy as np
 
-# Define the round constants as per the ZoKrates code
 ROUND_CONSTANTS = [
     42, 43, 170, 2209, 16426, 78087, 279978, 823517, 2097194, 4782931,
     10000042, 19487209, 35831850, 62748495, 105413546, 170859333,
@@ -16,33 +15,47 @@ ROUND_CONSTANTS = [
     3142742835999, 3521614606250, 3938980639125
 ]
 
-# SNARK_SCALAR_FIELD is not explicitly defined in your code snippet,
-# assuming it's a placeholder for a prime field order used in SNARKs
 SNARK_SCALAR_FIELD = 21888242871839275222246405745257275088548364400416034343698204186575808495617
 
+def convert_matrix(m):
+    m = np.array(m, dtype=np.int64)  # suitable data type for large numbers
+    # ensure values are within the field range
+    # adjusted_values = np.mod(m, SNARK_SCALAR_FIELD)
+    adjusted_values = np.where(m < 0, SNARK_SCALAR_FIELD + m, m)
+    sign_mask = np.where(m > 0, 0, 1)  # This remains unchanged
+    return adjusted_values, sign_mask
+
 def mimc(x, k, e=7, R=64):
-    """
-    Adjusted MiMC encryption function to match the ZoKrates implementation.
-    """
     for i in range(R):
         c_i = ROUND_CONSTANTS[i]
+        # ensure the operation stays within field
         a = (x + k + c_i) % SNARK_SCALAR_FIELD
         x = pow(a, e, SNARK_SCALAR_FIELD)
     return (x + k) % SNARK_SCALAR_FIELD
 
-def mimc_hash(inputs, k=0, e=7, R=64):
-    """
-    Hash function using the adjusted MiMC encryption to process inputs.
-    """
-    for input in inputs:
-        k = mimc(input, k, e, R)
+def mimc_hash(w: np.ndarray, b: np.ndarray, k=0, e=7, R=64):
+    global_weights, _ = convert_matrix(w)
+    print("Global Weights:", global_weights)
+    global_bias, _ = convert_matrix(b)
+    print("global_bias:", global_bias)
+
+    for i in range(len(global_weights)):
+        for j in range(global_weights[i].size): 
+            k = mimc(global_weights[i][j], k, e, R)
+        k = mimc(global_bias[i], k, e, R)
+
     return k
 
-# Example usage:
-if __name__ == "__main__":
-    # Input similar to ZoKrates preimage
-    inputs = [1, 2]  # Example input
 
-    # Compute the hash
-    hash_result = mimc_hash(inputs)
+
+if __name__ == "__main__":
+    w = [[1, -2], [3, -4]]
+    b = [1, 2]
+
+    # Adjust inputs to numpy arrays for processing
+    w_np = np.array(w)
+    b_np = np.array(b)
+
+    # Compute the hash considering the adjusted values
+    hash_result = mimc_hash(w_np, b_np)
     print(f"MiMC hash result: {hash_result}")
