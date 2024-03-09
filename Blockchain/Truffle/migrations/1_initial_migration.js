0c2e6db3a8a9e716653812e5d3ede34e6898b037
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2024-03-09 14:21:02 +0100

    UPD

--- Blockchain/Truffle/migrations/1_initial_migration.js
+++ Blockchain/Truffle/migrations/1_initial_migration.js
@@ -1,18 +1,21 @@
 const Migrations = artifacts.require("Migrations");
 const FederatedModel = artifacts.require("FederatedModel")
 const verifier = artifacts.require("Verifier")
+const verifier_aggregator = artifacts.require("VerifierAggregator")
 const fs = require('fs');
 const yaml = require('js-yaml');
 
 module.exports = function (deployer) {
-  //let fileContents = fs.readFileSync('/D:/Advancing-Blockchain-Based-Federated-Learning-Through-Verifiable-Off-Chain-Computations/CONFIG.yaml', 'utf8');
-  const fs = require('fs');
-  const configPath = 'D:\\Advancing-Blockchain-Based-Federated-Learning-Through-Verifiable-Off-Chain-Computations\\CONFIG.yaml';
-  const fileContents = fs.readFileSync(configPath, 'utf8');
+  let fileContents = fs.readFileSync('../../CONFIG.yaml', 'utf8');
+  
+ //let fileContents = fs.readFileSync('/D:/Advancing-Blockchain-Based-Federated-Learning-Through-Verifiable-Off-Chain-Computations/CONFIG.yaml', 'utf8');
+ //const fs = require('fs');
+ //const configPath = 'D:\\Advancing-Blockchain-Based-Federated-Learning-Through-Verifiable-Off-Chain-Computations\\CONFIG.yaml';
+ //const fileContents = fs.readFileSync(configPath, 'utf8');
+
   let data = yaml.load(fileContents);
   deployer.deploy(Migrations);
   deployer.deploy(FederatedModel, data.DEFAULT.InputDimension, data.DEFAULT.OutputDimension, data.DEFAULT.LearningRate, data.DEFAULT.Precision, data.DEFAULT.BatchSize, data.DEFAULT.IntervalTime);
   deployer.deploy(verifier, { gas: data.DEFAULT.Gas });
+  deployer.deploy(verifier_aggregator, { gas: data.DEFAULT.Gas });
 };
-
-
