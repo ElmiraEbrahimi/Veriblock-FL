c44826c9c675b5a7b093259252533f02ca563894
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2024-04-05 13:45:37 +0200

    UPD

--- Blockchain/Truffle/migrations/1_initial_migration.js
+++ Blockchain/Truffle/migrations/1_initial_migration.js
@@ -7,12 +7,6 @@ const yaml = require('js-yaml');
 
 module.exports = function (deployer) {
   let fileContents = fs.readFileSync('../../CONFIG.yaml', 'utf8');
-  
- //let fileContents = fs.readFileSync('/D:/Advancing-Blockchain-Based-Federated-Learning-Through-Verifiable-Off-Chain-Computations/CONFIG.yaml', 'utf8');
- //const fs = require('fs');
- //const configPath = 'D:\\Advancing-Blockchain-Based-Federated-Learning-Through-Verifiable-Off-Chain-Computations\\CONFIG.yaml';
- //const fileContents = fs.readFileSync(configPath, 'utf8');
-
   let data = yaml.load(fileContents);
   deployer.deploy(Migrations);
   deployer.deploy(FederatedModel, data.DEFAULT.InputDimension, data.DEFAULT.OutputDimension, data.DEFAULT.LearningRate, data.DEFAULT.Precision, data.DEFAULT.BatchSize, data.DEFAULT.IntervalTime);
