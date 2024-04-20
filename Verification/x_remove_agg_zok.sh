0984b6a09c12b3ebee811385e3434c3467bb1c3f
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2024-04-20 21:28:07 +0200

    ADD

---
+++ Verification/x_remove_agg_zok.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm "./zokrates/aggregator/abi.json"
+rm "./zokrates/aggregator/proof.json"
+rm "./zokrates/aggregator/proving.key"
+rm "./zokrates/aggregator/verification.key"
+rm "./zokrates/aggregator/verifier.sol"
+rm "./zokrates/aggregator/witness"
+rm "./zokrates/aggregator/out"
+rm "./zokrates/aggregator/out.r1cs"
+rm "./zokrates/aggregator/proof_aggregator"
+rm "./zokrates/aggregator/witness_aggregator"
