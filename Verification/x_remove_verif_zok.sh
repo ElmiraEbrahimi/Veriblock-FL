f3188f71935b86d5a48e0163acaa05122167270d
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2024-04-20 21:27:32 +0200

    ADD

---
+++ Verification/x_remove_verif_zok.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm "./abi.json"
+rm "./proof.json"
+rm "./proving.key"
+rm "./verification.key"
+rm "./verifier.sol"
+rm "./witness"
+rm "./root"
+rm "./root.r1cs"
+rm "./out.r1cs"
+rm "./out"
+rm ./proof_*
+rm ./witness_*
+
+rm ./../out.wtns
