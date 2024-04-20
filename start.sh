43a9b455fcb166469c28873d36239ace021c0f2c
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2024-04-20 20:52:12 +0200

    [UPD] 15 accounts

--- start.sh
+++ start.sh
@@ -1,19 +1,30 @@
 # #!/bin/bash
 
-# export NODE_OPTIONS=--max-old-space-size=20480
-# ganache-cli --gasLimit=0x1fffffffffffff  --allowUnlimitedContractSize -e 1000000000
-
-
+# remove old docker containers
 container_ids=$(sudo docker ps -q)
 for container_id in $container_ids
 do
     sudo docker rm -f $container_id
 done
-sudo docker run -d -p 8545:8545 trufflesuite/ganache:v7.0.0 --miner.blockGasLimit=0x1fffffffffffff  --chain.allowUnlimitedContractSize=true --wallet.defaultBalance 1000000000
+
+# restart docker containers
+sudo docker run -d -p 8545:8545 trufflesuite/ganache:v7.0.0 --miner.blockGasLimit=0x1fffffffffffff  --chain.allowUnlimitedContractSize=true --wallet.defaultBalance 1000000000 --accounts 15
 sudo docker run -d --name rabbitmq -p 5672:5672 -p 5673:5673 -p 15672:15672 rabbitmq:3-management
 
+# recompile/remigrate smart contract files
 cd blockchain/Truffle/
 sudo rm -rf build/contracts/*
-sudo truffle compile
-sudo truffle migrate
-cd ...
+truffle compile
+truffle migrate
+cd ./../..
+echo "Current directory: $(pwd)"
+
+# remove balance.csv:
+balance_csv="balance.csv"
+echo "Deleting $balance_csv ..."
+if [ -f "$balance_csv" ]; then
+    sudo rm "${balance_csv}"
+    echo "Done"
+else
+    echo "$balance_csv was not found in directory."
+fi
